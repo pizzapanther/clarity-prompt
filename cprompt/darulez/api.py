9d8e9e554516360cbc789cd1ebc6ebbf7b81e688
Paul Bailey <paul@neutron.studio>
2024-05-23 00:06:10 -0500

    tweaks

--- cprompt/darulez/api.py
+++ cprompt/darulez/api.py
@@ -5,15 +5,18 @@ from cprompt.darulez.models import DocumentRule, School
 
 router = Router()
 
+
 class DocRuleScheam(ModelSchema):
   class Meta:
     model = DocumentRule
     fields = ['id', 'school', 'action', 'conditions']
 
+
 class AddDocRuleScheam(ModelSchema):
   class Meta:
     model = DocumentRule
     fields = ['school', 'action', 'conditions']
+
 
 @router.post("/rule/add", response=DocRuleScheam)
 def add_rule(request, data: AddDocRuleScheam):
